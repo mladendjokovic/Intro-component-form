dfddaa9d8213cb376a9e4e3db1f2c8cce3061c35
mladendjokovic <mldndjkvc@gmail.com>
2020-04-10 22:16:57 +0200

    fixing the form submission

--- script.js
+++ script.js
@@ -8,55 +8,50 @@ let emailLabel = document.querySelector(".email > p")
 let passwordLabel = document.querySelector(".password > p")
 const formButton = document.getElementById("button")
 const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-function check() {
-    firstNameCheck()
-    lastNameCheck()
-    emailCheck()
-    passwordCheck()
-}
 
-function firstNameCheck() {
+
+function check(event) {
     if(document.getElementById("first-name").value == "") {
         firstImg.style.display="unset"
         firstLabel.textContent="First Name can't be empty"
         document.getElementById("first-name").style.borderColor = "hsl(0, 100%, 74%)"
-        return false
     }
     else {
-        return true
+        document.getElementById("first-name").style.borderColor = "hsl(246, 25%, 77%)"
+        firstLabel.textContent =""
+        firstImg.style.display = "none"
     }
-}
-function lastNameCheck() {
     if(document.getElementById("last-name").value == "") {
         lastImg.style.display="unset"
         lastLabel.textContent="First Name can't be empty"
         document.getElementById("last-name").style.borderColor = "hsl(0, 100%, 74%)"
-        return false
     }
     else {
-        return true
+        lastImg.style.display="none"
+        lastLabel.textContent=""
+        document.getElementById("last-name").style.borderColor = "hsl(246, 25%, 77%)"
     }
-}
-function emailCheck() {
+    if(document.getElementById("password").value == "") {
+        passwordImg.style.display="unset"
+        passwordLabel.textContent="Password can't be empty"
+        document.getElementById("password").style.borderColor = "hsl(0, 100%, 74%)"
+        }
+    else{
+        passwordImg.style.display = "none"
+        passwordLabel.textContent = ""
+        document.getElementById("password").style.borderColor = "hsl(246, 25%, 77%)"
+    }
     if(pattern.test(document.getElementById("email").value)) {
-        return true
+        emailImg.style.display = "none"
+        emailLabel.textContent = ""
+        document.getElementById("email").style.borderColor = "hsl(246, 25%, 77%)"
     }
     else {
         emailLabel.textContent = "Not a valid email"
         emailImg.style.display = "unset"
         document.getElementById("email").style.borderColor = "hsl(0, 100%, 74%)"
-        return false
     }
+    event.preventDefault()
 }
+//formButton.addEventListener("submit", check())
 
-function passwordCheck() {
-    if(document.getElementById("password").value == "") {
-        passwordImg.style.display="unset"
-        passwordLabel.textContent="Password can't be empty"
-        document.getElementById("password").style.borderColor = "hsl(0, 100%, 74%)"
-        return false
-    }
-    else {
-        return true
-    }
-}
