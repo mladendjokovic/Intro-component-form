0a727b11134b216917d7fbb0a934dffbde378018
mladendjokovic <mldndjkvc@gmail.com>
2020-06-01 02:40:23 +0200

    second commit - from scratch

---
+++ app.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll('.text-input input');
+const emailInput = document.querySelector('.email input');
+const emailError = document.querySelector('.email h3');
+const message = document.querySelectorAll('.text-input h3');
+const img = document.querySelectorAll('.text-input img');
+const btn = document.querySelector('.form button');
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    inputs.forEach( (input,index) => {
+        const name = input.placeholder;
+        if(input.value.length === 0) {
+            message[index].innerHTML = `${name} cannot be empty`;
+            img[index].classList.add('active');
+        } else {
+            message[index].innerHTML = "";
+            img[index].classList.remove('active')
+        }
+    })
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+        document.querySelector('.email img').classList.add('active');
+        emailError.innerHTML = "Looks like this is not an email";
+    } else {
+        document.querySelector('.email img').classList.remove('active');
+        emailError.innerHTML = "";
+    }
+})
